667eda110d0e79814971e72717eb03716032fbce
MauLang18 <maulangbonilla.18@gmail.com>
2023-10-11 08:03:52 -0600

    Mejoras en el Login

--- StockView/StockView/Views/LoginPage.xaml.cs
+++ StockView/StockView/Views/LoginPage.xaml.cs
@@ -1,5 +1,5 @@
 ﻿using StockView.ViewModel;
-
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,6 +12,33 @@ namespace StockView.Views
         {
             InitializeComponent();
             BindingContext = new LoginPageViewModel(Navigation);
+            txtUsuario.Completed += OnUsuarioEntryCompleted;
+            txtContrasena.Completed += OnContrasenaEntryCompleted;
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            // Selecciona automáticamente el texto en el control Entry
+            txtUsuario.Focus();
+        }
+
+        private void OnUsuarioEntryCompleted(object sender, EventArgs e)
+        {
+            // Mueve el foco al siguiente Entry (txtContrasena)
+            txtContrasena.Focus();
+        }
+
+        private async void OnContrasenaEntryCompleted(object sender, EventArgs e)
+        {
+            // Aquí puedes llamar al método para iniciar sesión
+            if (BindingContext is LoginPageViewModel viewModel)
+            {
+                // Accede al comando de inicio de sesión y ejecútalo
+                if (viewModel.LoginCommand.CanExecute(null))
+                    viewModel.LoginCommand.Execute(null);
+            }
         }
     }
 }
